9cae838d25d2c03db1eef7ec9bdf30b0b8081886
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-12-27 15:17:43 +0300

    Внесены правки

--- src/__tests__/test.validateUsername.js
+++ src/__tests__/test.validateUsername.js
@@ -1,61 +1,55 @@
 import Validator from '../js/Validator';
 
 test('validation of entered symbols', () => {
-  expect(new Validator().validateUsername('qw-er123t_ykld')).toEqual('Имя соответсвует требованиям');
+  expect(Validator.validateUsername('qw-er123t_ykld')).toEqual(
+    'Имя соответсвует требованиям',
+  );
 });
 
 test('validation of entered symbols', () => {
-  expect(() => {
-    const name = new Validator();
-    return name.validateUsername('kdfs4свьм');
-  }).toThrow(new Error('Имя может содержатть только латинские буквы,цифры, дефис и нижнее подчеркивание'));
+  expect(Validator.validateUsername('kdfs4свьм')).toEqual(
+    'Имя может содержатть только латинские буквы,цифры, дефис и нижнее подчеркивание',
+  );
 });
 
 test('validation of the number of digits', () => {
-  expect(() => {
-    const name = new Validator();
-    return name.validateUsername('kdfs4444dsd');
-  }).toThrow(new Error('Имя может содержать не более 3х цифр подряд'));
+  expect(Validator.validateUsername('kdfs4444dsd')).toEqual(
+    'Имя может содержать не более 3х цифр подряд',
+  );
 });
 
 test('validation of the first symbol entered', () => {
-  expect(() => {
-    const name = new Validator();
-    return name.validateUsername('1kdfs4dsd');
-  }).toThrow(new Error('Имя не может начинаться с цифры, нижнего подчеркивания или дефиса'));
+  expect(Validator.validateUsername('1kdfs4dsd')).toEqual(
+    'Имя не может начинаться с цифры, нижнего подчеркивания или дефиса',
+  );
 });
 
 test('validation of the first symbol entered', () => {
-  expect(() => {
-    const name = new Validator();
-    return name.validateUsername('-kdfs4dsd');
-  }).toThrow(new Error('Имя не может начинаться с цифры, нижнего подчеркивания или дефиса'));
+  expect(Validator.validateUsername('-kdfs4dsd')).toEqual(
+    'Имя не может начинаться с цифры, нижнего подчеркивания или дефиса',
+  );
 });
 
 test('validation of the first symbol entered', () => {
-  expect(() => {
-    const name = new Validator();
-    return name.validateUsername('_kdfs4dsd');
-  }).toThrow(new Error('Имя не может начинаться с цифры, нижнего подчеркивания или дефиса'));
+  expect(Validator.validateUsername('_kdfs4dsd')).toEqual(
+    'Имя не может начинаться с цифры, нижнего подчеркивания или дефиса',
+  );
 });
 
 test('validation of the last entered symbol', () => {
-  expect(() => {
-    const name = new Validator();
-    return name.validateUsername('kdfs4dsd1');
-  }).toThrow(new Error('Имя не может заканчиваться на цифру, нижнее подчеркивание или дефис'));
+  expect(Validator.validateUsername('kdfs4dsd1')).toEqual(
+    'Имя не может заканчиваться на цифру, нижнее подчеркивание или дефис',
+  );
 });
 
 test('validation of the last entered symbol', () => {
-  expect(() => {
-    const name = new Validator();
-    return name.validateUsername('kdfs4dsd-');
-  }).toThrow(new Error('Имя не может заканчиваться на цифру, нижнее подчеркивание или дефис'));
+  expect(Validator.validateUsername('kdfs4dsd-')).toEqual(
+    'Имя не может заканчиваться на цифру, нижнее подчеркивание или дефис',
+  );
 });
 
 test('validation of the last entered symbol', () => {
-  expect(() => {
-    const name = new Validator();
-    return name.validateUsername('kdfs4dsd_');
-  }).toThrow(new Error('Имя не может заканчиваться на цифру, нижнее подчеркивание или дефис'));
+  expect(Validator.validateUsername('kdfs4dsd_')).toEqual(
+    'Имя не может заканчиваться на цифру, нижнее подчеркивание или дефис',
+  );
 });
