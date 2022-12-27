2c510d5e88dd11b05c3e7f3d74ffa663a4f2e319
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-12-27 15:46:36 +0300

    Внесены правки в структуру тестов, тесты переименованы

--- src/__tests__/test.validateUsername.js
+++ src/__tests__/test.validateUsername.js
@@ -1,55 +1,64 @@
 import Validator from '../js/Validator';
 
-test('validation of entered symbols', () => {
-  expect(Validator.validateUsername('qw-er123t_ykld')).toEqual(
+test('validation when validating the correct name', () => {
+  const received = Validator.validateUsername('qw-er123t_ykld');
+  expect(received).toEqual(
     'Имя соответсвует требованиям',
   );
 });
 
-test('validation of entered symbols', () => {
-  expect(Validator.validateUsername('kdfs4свьм')).toEqual(
+test('validation when checking an incorrect name (Cyrillic)', () => {
+  const received = Validator.validateUsername('kdfs4свьм');
+  expect(received).toEqual(
     'Имя может содержатть только латинские буквы,цифры, дефис и нижнее подчеркивание',
   );
 });
 
-test('validation of the number of digits', () => {
-  expect(Validator.validateUsername('kdfs4444dsd')).toEqual(
+test('validation when entering more than the allowed number of digits', () => {
+  const received = Validator.validateUsername('kdfs4444dsd');
+  expect(received).toEqual(
     'Имя может содержать не более 3х цифр подряд',
   );
 });
 
-test('validation of the first symbol entered', () => {
-  expect(Validator.validateUsername('1kdfs4dsd')).toEqual(
+test('checking when entering the first digit symbol', () => {
+  const received = Validator.validateUsername('1kdfs4dsd');
+  expect(received).toEqual(
     'Имя не может начинаться с цифры, нижнего подчеркивания или дефиса',
   );
 });
 
-test('validation of the first symbol entered', () => {
-  expect(Validator.validateUsername('-kdfs4dsd')).toEqual(
+test('checking when entering the first dash symbol', () => {
+  const received = Validator.validateUsername('-kdfs4dsd');
+  expect(received).toEqual(
     'Имя не может начинаться с цифры, нижнего подчеркивания или дефиса',
   );
 });
 
-test('validation of the first symbol entered', () => {
-  expect(Validator.validateUsername('_kdfs4dsd')).toEqual(
+test('checking when entering the first symbol is an underscore', () => {
+  const received = Validator.validateUsername('_kdfs4dsd');
+  expect(received).toEqual(
     'Имя не может начинаться с цифры, нижнего подчеркивания или дефиса',
   );
 });
 
-test('validation of the last entered symbol', () => {
-  expect(Validator.validateUsername('kdfs4dsd1')).toEqual(
+test('checking when entering the last digit symbol', () => {
+  const received = Validator.validateUsername('kdfs4dsd1');
+  expect(received).toEqual(
     'Имя не может заканчиваться на цифру, нижнее подчеркивание или дефис',
   );
 });
 
-test('validation of the last entered symbol', () => {
-  expect(Validator.validateUsername('kdfs4dsd-')).toEqual(
+test('checking when entering the last dash symbol', () => {
+  const received = Validator.validateUsername('kdfs4dsd-');
+  expect(received).toEqual(
     'Имя не может заканчиваться на цифру, нижнее подчеркивание или дефис',
   );
 });
 
-test('validation of the last entered symbol', () => {
-  expect(Validator.validateUsername('kdfs4dsd_')).toEqual(
+test('the check when entering the last symbol is an underscore', () => {
+  const received = Validator.validateUsername('kdfs4dsd_');
+  expect(received).toEqual(
     'Имя не может заканчиваться на цифру, нижнее подчеркивание или дефис',
   );
 });
